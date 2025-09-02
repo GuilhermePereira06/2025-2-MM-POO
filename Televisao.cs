c763d033362d5f9ff846a1dc1d30728d8ed025bf
Guilherme Pereira da Silva <164202007+GuilhermePereira06@users.noreply.github.com>
2025-09-02 12:50:25 +0000

    Atualização da aula

---
+++ Televisao.cs
@@ -0,0 +1,8 @@
+public class Televisao(){
+    public float Tamannho { get; set;}
+    public int Volume {get; set;}
+    public int Canal {get; set;}
+    public bool Ligada { get; set; }
+    public string Resolucao { get; set; }
+
+}
